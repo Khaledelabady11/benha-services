3435651e9dd97bcbc3715d634aeb2905ee074d0f
Khaled Mohamed <kmohamed9231@gmail.com>
2021-08-23 18:21:57 +0200

    ههههههههههههههههههههههههههههههههههه

--- app/src/main/java/com/example/banha_services/MainActivity.java
+++ app/src/main/java/com/example/banha_services/MainActivity.java
@@ -1,51 +1,62 @@
 package com.example.banha_services;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
 
-import androidx.annotation.NonNull;
+
+import android.view.Menu;
+
+import android.view.MenuItem;
+
+
+import android.widget.SearchView;
+
+
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.SearchView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
-RecyclerView recyclerView;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       loadLocale();
         setContentView(R.layout.activity_main);
-        List<recyclerData> arrayList=new ArrayList<>();
-        recyclerData data2=new recyclerData();
+
+        List<recyclerData> arrayList = new ArrayList<>();
+        recyclerData data2 = new recyclerData();
         data2.setName("Medical");
         data2.setImage(R.drawable.medical);
-        recyclerData data3=new recyclerData();
+        recyclerData data3 = new recyclerData();
         data3.setName("medical");
         data3.setImage(R.drawable.medical);
-        recyclerData data4=new recyclerData();
+        recyclerData data4 = new recyclerData();
         data4.setName("Medical");
         data4.setImage(R.drawable.medical);
-        recyclerData data5=new recyclerData();
+        recyclerData data5 = new recyclerData();
         data5.setName("khaled");
         data5.setImage(R.drawable.medical);
-        recyclerData data6=new recyclerData();
+        recyclerData data6 = new recyclerData();
         data6.setName("khaled");
         data6.setImage(R.drawable.medical);
-        recyclerData data7=new recyclerData();
+        recyclerData data7 = new recyclerData();
         data7.setName("khaled");
         data7.setImage(R.drawable.medical);
-        recyclerData data8=new recyclerData();
+        recyclerData data8 = new recyclerData();
         data8.setName("khaled");
         data8.setImage(R.drawable.medical);
-        recyclerData data9=new recyclerData();
+        recyclerData data9 = new recyclerData();
         data9.setName("khaled");
         data9.setImage(R.drawable.medical);
-        recyclerData data10=new recyclerData();
+        recyclerData data10 = new recyclerData();
         data10.setName("khaled");
         data10.setImage(R.drawable.medical);
         arrayList.add(data2);
@@ -57,20 +68,17 @@ RecyclerView recyclerView;
         arrayList.add(data8);
         arrayList.add(data9);
         arrayList.add(data10);
-
-        recyclerView=findViewById(R.id.recycler);
-        recyclerAdapter adapter=new recyclerAdapter(arrayList,this);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView = findViewById(R.id.recycler);
+        recyclerAdapter adapter = new recyclerAdapter(arrayList, this);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         recyclerView.setAdapter(adapter);
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.change_language,menu);
-        MenuItem item=menu.findItem(R.id.search);
-        SearchView searchView=(SearchView) item.getActionView();
+        getMenuInflater().inflate(R.menu.change_language, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
         searchView.setQueryHint("type here to search");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -83,15 +91,38 @@ RecyclerView recyclerView;
                 return false;
             }
         });
-        
+
         return true;
     }
-
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
-        if (id==R.id.ar)
-            Toast.makeText(this,"Arabic ",Toast.LENGTH_LONG).show();
-        return super.onOptionsItemSelected(item);
+    public boolean onContextItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()){
+            case R.id.ar:
+                setLocale("ar");
+                recreate();
+            case R.id.en:
+                setLocale("en");
+                recreate();
+            default:
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Locale.setDefault(myLocale);
+        Configuration conf = new Configuration();
+        conf.locale = myLocale;
+        getBaseContext().getResources().updateConfiguration(conf,getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("settings",MODE_PRIVATE).edit();
+        editor.putString("My_Lang",lang);
+        editor.apply();
+     }
+     public  void loadLocale(){
+        SharedPreferences preferences = getSharedPreferences("settings", Activity.MODE_PRIVATE);
+        String language = preferences.getString("My_Lang","");
+        setLocale(language);
+     }
+
 }
