e61392f263920298f4b0f42d3bb4426746a21228
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2021-08-31 15:39:15 +0200

    Api Update

--- app/src/main/java/element/ElementActivity.java
+++ app/src/main/java/element/ElementActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ListView;
 
 import com.example.banha_services.R;
@@ -12,33 +16,56 @@ import com.example.banha_services.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import Firms.Firmsitem;
+import Firms.ApiFirm;
+import Firms.Firmsitem;
 import category.categoryAdapter;
-import category.categoryData;
 import element.elementAdapter;
 import element.elementData;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ElementActivity extends AppCompatActivity {
 
 private  RecyclerView element;
+    private static final String Base_Url="https://banha-services.herokuapp.com/api/";
+    Animation   animSlideUp;
+    private List<Firmsitem> firmList = new ArrayList<>();
 
 
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hosptal);
-
-    List<elementData> arrayList = new ArrayList<>();
-    elementData data2 = new elementData();
-    data2.setName("Medical");
-    data2.setPhone_number("01206458433");
-   data2.setDesc("hdhhdhd");
-   data2.setAddress("sdsd");
-    arrayList.add(data2);
-
+        setContentView(R.layout.activity_element);
+        Intent intent=getIntent();
+        int category_id=intent.getIntExtra("id",0);
     element=(RecyclerView) findViewById(R.id.element_recycler);
-    elementAdapter arrAdapter=new elementAdapter(arrayList,this);
-    element.setLayoutManager(new GridLayoutManager(this,1));
-    element.setAdapter(arrAdapter);
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(Base_Url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiFirm apiFirm=retrofit.create(ApiFirm.class);
+        Call<List<Firmsitem>>call=apiFirm.getFirm(category_id);
+        call.enqueue(new Callback<List<Firmsitem>>() {
+            @Override
+            public void onResponse(Call<List<Firmsitem>> call, Response<List<Firmsitem>> response) {
+                firmList=response.body();
+                elementAdapter adapter=new elementAdapter( firmList, ElementActivity.this);
+                element.setLayoutManager(new GridLayoutManager(ElementActivity.this, 1));
+                element.setAdapter(adapter);
+                animSlideUp= AnimationUtils.loadAnimation(getApplicationContext(), R.anim.up);
+                element.setVisibility(View.VISIBLE);
+                element.startAnimation(animSlideUp);
+            }
+
+            @Override
+            public void onFailure(Call<List<Firmsitem>> call, Throwable t) {
+
+            }
+        });
 
 
     }
